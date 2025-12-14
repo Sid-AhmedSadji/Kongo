1ef1361bd81bcac070af6b92e91623e2764a0595
Omer A <omergn69@gmail.com>
2025-12-14 18:06:22 +0100

    asdasdasda

--- kongo-backend/routes/apiRoutes.js
+++ kongo-backend/routes/apiRoutes.js
@@ -8,7 +8,7 @@ const reservationController = require('../controllers/reservationController');
 // Routes Newsletter
 router.post('/newsletter', newsletterController.newsletter);
 
-// Routes Réservation (URL mise à jour : /reservations)
+// Routes Réservation (URL mise à jour : /reservation)
 router.post('/reservation', reservationController.createReservation);
 
 module.exports = router;

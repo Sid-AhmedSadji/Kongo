b7273f2d2fe624d30cfcc1cce071bcdae1ecc095
Omer A <omergn69@gmail.com>
2025-12-14 16:33:42 +0100

    asdadasd

--- kongo-backend/middleware/globalMiddleware.js
+++ kongo-backend/middleware/globalMiddleware.js
@@ -2,15 +2,27 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const globalMiddleware = (app) => {
+  // Logging méthode + URL
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 
-    app.use(
-        () => {console.log(origine)}
-    )
+  // Log de l'origine de la requête
+  app.use((req, res, next) => {
+    const origin = req.headers.origin || 'unknown-origin';
+    console.log('Origin:', origin);
+    next();
+  });
 
-    app.use(helmet());
-    app.use(cors({
-        origin : 'http://localhost:4200'
-    }));
+  // Sécurité HTTP headers
+  app.use(helmet());
+
+  // CORS pour le front Angular en dev
+  app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  }));
 };
 
 module.exports = globalMiddleware;

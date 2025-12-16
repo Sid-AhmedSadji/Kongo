83ea36d32af0ef65af234e7bbfbbddb782081844
Sid-AhmedSadji <sid-ahmed_sadji@hotmail.com>
2025-12-16 14:22:24 +0100

    user

--- kongo-backend/routes/apiRoutes.js
+++ kongo-backend/routes/apiRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const newsletterController = require('../controllers/newsletterController');
 const reservationController = require('../controllers/reservationController');
 const adminController = require('../controllers/adminController');
+const userController = require('../controllers/userController');
 
 // Routes Newsletter
 router.post('/newsletter', newsletterController.newsletter);
@@ -13,5 +14,12 @@ router.post('/reservation', reservationController.createReservation);
 
 // Routes Admin
 router.get('/admin', adminController.getAdmin);
+
+// Routes User
+route .post('/signup', userController.signup);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/user', userController.getUser);
+router.get('/creditCard', userController.getCreditCard);
 
 module.exports = router;

27977118801a03e6e8fc781948ed910cbfee473c
Omer A <omergn69@gmail.com>
2025-12-16 11:46:18 +0100

    asd;mjnaosdhas

--- kongo-backend/middleware/globalMiddleware.js
+++ kongo-backend/middleware/globalMiddleware.js
@@ -15,10 +15,17 @@ const globalMiddleware = (app) => {
   app.use(helmet());
 
   // CORS pour le front Angular en dev
-  app.use(cors({
-    origin: 'http://localhost:4200',
-    credentials: true
-  }));
-};
+const allowedOrigins = ['http://localhost:4200', 'http://192.158.50.20:4200'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true); // origin autorisée
+    } else {
+      callback(new Error('Origin non autorisée')); // origin manquante ou non autorisée
+    }
+  },
+  credentials: true
+}));
+
 
 module.exports = globalMiddleware;

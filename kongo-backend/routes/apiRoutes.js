181c7e1ecdf9ec3bc627939114ffc678e56c706c
Sid-AhmedSadji <sid-ahmed_sadji@hotmail.com>
2025-12-16 14:22:35 +0100

    user

--- kongo-backend/routes/apiRoutes.js
+++ kongo-backend/routes/apiRoutes.js
@@ -16,7 +16,7 @@ router.post('/reservation', reservationController.createReservation);
 router.get('/admin', adminController.getAdmin);
 
 // Routes User
-route .post('/signup', userController.signup);
+router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/user', userController.getUser);
